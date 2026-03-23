ec5974d1e17ba13122777e735866b3a4c46b3e53
juliencohen <Julien.Cohen@univ-nantes.fr>
2026-03-23 16:33:26 +0100

    renaming.

--- src/main/java/org/example/dto/ExpressionBuilder.java
+++ src/main/java/org/example/dto/ExpressionBuilder.java
@@ -9,20 +9,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ExpressionBuilder {
+
     public static org.example.dto.Expression transpile(org.omg.sysml.lang.sysml.Expression e){
         switch (e) {
-            case FeatureReferenceExpression r :
-                throw new InvalidParameterException("FeatureReferenceExpression not tested.");
 
             case FeatureChainExpression c :
-                List<Relationship> identifiers =  c.getTargetFeature().getOwnedRelationship();
-                List<String> id2 = identifiers.stream().map((f)->f.getTarget().getFirst().getDeclaredName()).toList();
+                List<Relationship> references =  c.getTargetFeature().getOwnedRelationship();
+                List<String> names =
+                        references.stream().map((f)->f.getTarget().getFirst().getDeclaredName())
+                                .toList();
                 String outername =
                         c.getTargetFeature().getOwnedRelationship().get(1)
-                                .getTarget().getFirst().getOwningNamespace().getOwningNamespace().getDeclaredName() ; // fixme
-                List<String> id3 = new LinkedList<>(id2);
-                id3.addFirst(outername);
-                return new DotExpression(id3);
+                                .getTarget().getFirst().getOwningNamespace().getOwningNamespace()
+                                .getDeclaredName() ; // fixme
+                List<String> all_names = new LinkedList<>(names);
+                all_names.addFirst(outername);
+                return new DotExpression(all_names);
+
+            case FeatureReferenceExpression r :
+                throw new InvalidParameterException("FeatureReferenceExpression not tested.");
 
             case LiteralString s :
                 throw new InvalidParameterException("String Literals cannot be used as loop conditions. (" + s.getValue() + ")");
