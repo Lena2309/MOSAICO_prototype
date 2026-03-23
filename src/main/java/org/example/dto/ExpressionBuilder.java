3f40abde6fc7638ed7bfd8232d0f00cd194fe1fb
juliencohen <Julien.Cohen@univ-nantes.fr>
2026-03-23 09:50:50 +0100

    Fix : first identifier of the FeatureChain extracted.

--- src/main/java/org/example/dto/ExpressionBuilder.java
+++ src/main/java/org/example/dto/ExpressionBuilder.java
@@ -4,6 +4,8 @@ package org.example.dto;
 import org.omg.sysml.lang.sysml.*;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ExpressionBuilder {
@@ -15,7 +17,12 @@ public class ExpressionBuilder {
             case FeatureChainExpression c :
                 List<Relationship> identifiers =  c.getTargetFeature().getOwnedRelationship();
                 List<String> id2 = identifiers.stream().map((f)->f.getTarget().getFirst().getDeclaredName()).toList();
-                return new DotExpression(id2);
+                String outername =
+                        c.getTargetFeature().getOwnedRelationship().get(1)
+                                .getTarget().getFirst().getOwningNamespace().getOwningNamespace().getDeclaredName() ; // fixme
+                List<String> id3 = new LinkedList<>(id2);
+                id3.addFirst(outername);
+                return new DotExpression(id3);
 
             case LiteralString s :
                 throw new InvalidParameterException("String Literals cannot be used as loop conditions. (" + s.getValue() + ")");
