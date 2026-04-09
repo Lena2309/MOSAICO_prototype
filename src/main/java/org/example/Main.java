00cee34d9de5a2b02936fb60d3c5f92d10f1496d
juliencohen <Julien.Cohen@univ-nantes.fr>
2026-04-09 16:31:19 +0200

    Fix.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Hello and welcome!"));
 
-        //var parsed = SysMLDecoder.decode("src/main/resources/req1.sysml");
-        var parsed = SysMLDecoder.decode("src/main/resources/test_cases/test4.sysml");
+        var parsed = SysMLDecoder.decode("src/main/resources/req1.sysml");
+        //var parsed = SysMLDecoder.decode("src/main/resources/test_cases/test4.sysml");
         System.out.println(parsed.toString());
         var collabAgent = new CollaborationAgent();
         var outputsString = collabAgent.run(parsed);
