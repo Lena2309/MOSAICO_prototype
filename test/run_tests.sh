e00ff9268788c759be8fab55f3a5945f7f701e37
juliencohen <Julien.Cohen@univ-nantes.fr>
2026-04-15 13:19:59 +0200

    FIX: test automation.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,17 +6,35 @@ CPT=0
 
 cd ..
 
-FILES=("test/test_cases/test0.sysml" "test/test_cases/test0_2.sysml" "test/test_cases/test0_3.sysml" "test/test_cases/test1.sysml" "test/test_cases/test2.sysml" "test/test_cases/test3.sysml" "test/test_cases/test3.2.sysml" "test/test_cases/test3.3.sysml" "test/test_cases/test4.sysml" "test/test_cases/test5.sysml" "test/test_cases/test5_2.sysml" "test/test_cases/test6.sysml" "test/test_cases/test7.sysml")
+FILES=("test/test_cases/test0.sysml" "test/test_cases/test1.sysml" "test/test_cases/test2.sysml"  "test/test_cases/test3.2.sysml" "test/test_cases/test4.sysml" "test/test_cases/test5.sysml" "test/test_cases/test5_2.sysml" "test/test_cases/test6.sysml" "test/test_cases/test7.sysml")
+NON_TERMINATION_FILES=( "test/test_cases/test0_2.sysml" "test/test_cases/test0_3.sysml" "test/test_cases/test3.3.sysml" "test/test_cases/test3.sysml")
 
+# Tests that produce OK or KO
 for f in "${FILES[@]}"
 do
-  echo TESTING $f $CPT
-  mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args=$f > out.$CPT
-  if  grep OUTPUT out.$CPT | grep "Test OK" ; then
+  echo $CPT: TESTING $f
+  mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args=$f &> out.$CPT
+  if  grep OUTPUT out.$CPT | grep "Test KO" ; then
+      echo FAILED
+      FAILED=$(( FAILED + 1 ))
+  elif  grep OUTPUT out.$CPT | grep "Test OK" ; then
+    echo PASSED
     PASSED=$(( PASSED + 1 ))
   fi
-  if  grep OUTPUT out.$CPT | grep "Test KO" ; then
+   CPT=$(( CPT + 1 ))
+done
+
+# Tests that produce KO or nothing
+for f in "${NON_TERMINATION_FILES[@]}"
+do
+  echo $CPT: TESTING $f
+  mvn compile exec:java -Dexec.mainClass="org.example.Main" -Dexec.args=$f &> out.$CPT
+  if grep OUTPUT out.$CPT | grep "Test KO" ; then
+    echo FAILED
     FAILED=$(( FAILED + 1 ))
+  else
+    echo PASSED
+    PASSED=$(( PASSED + 1 ))
   fi
    CPT=$(( CPT + 1 ))
 done
